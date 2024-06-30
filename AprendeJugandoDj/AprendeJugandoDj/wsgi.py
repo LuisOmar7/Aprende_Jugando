417056deec3593a2ebf7d6087a7610da79b97097
LuisOmar7 <luisomaravalosortiz.77@gmail.com>
2024-06-30 15:05:08 -0600

    Agregando proyecto prueba

---
+++ AprendeJugandoDj/AprendeJugandoDj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AprendeJugandoDj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AprendeJugandoDj.settings')
+
+application = get_wsgi_application()
